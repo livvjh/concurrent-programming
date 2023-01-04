016662c34032ce57c4dc74af08be2a779d4c947b
livvjh <livejh7129@gmail.com>
2023-01-05 07:09:04 +0900

    코루틴 차이 개념 추가

--- bound/io_bound.py
+++ bound/io_bound.py
@@ -1,7 +1,7 @@
 def io_bound_func():
     print("값을 입력해주세요.")
     # 사용자가 입력을 안하는 경우 I/O 바운드 발생
-    # 이때 사용자가 아니고 서버끼리 통신시에도 발생
+    # 이때 사용자가 아니고 서버끼리 통신시에도 발생 (서버에서 응답이 안오면 그걸 기다리는 상태로 병목)
     input_value = input()
     return int(input_value) + 100
 
